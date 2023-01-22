694e237e53417c6599226064c4252e9c2a9f41fd
loky13 <loky13@daum.net>
2023-01-22 15:04:31 +0900

    compositio api custom hook
    https://github.com/xxx-sj/Today_I_Learned/tree/master/vue/mixins/composition_api

--- vue/complete_guide/reuse-07-example-starting-project/src/components/projects/ProjectsList.vue
+++ vue/complete_guide/reuse-07-example-starting-project/src/components/projects/ProjectsList.vue
@@ -13,9 +13,10 @@
 </template>
 
 <script>
-import { ref, computed, watch, toRefs } from 'vue';
+import {computed, watch, toRefs } from 'vue';
 
 import ProjectItem from './ProjectItem.vue';
+import useSearch from "@/hooks/search";
 
 export default {
   components: {
@@ -23,46 +24,54 @@ export default {
   },
   props: ['user'],
   setup(props) {
-    const enteredSearchTerm = ref('');
-    const activeSearchTerm = ref('');
 
-    const availableProjects = computed(function () {
-      if (activeSearchTerm.value) {
-        return props.user.projects.filter((prj) =>
-          prj.title.includes(activeSearchTerm.value)
-        );
-      }
-      return props.user.projects;
-    });
+    const { user } = toRefs(props);
+
+    const projects = computed(function() {
+      return user.value ? user.value.projects : [];
+    })
+
+    const {enteredSearchTerm, availableItems, updateSearch} = useSearch(
+        projects,
+        'title');
+    // const enteredSearchTerm = ref('');
+    // const activeSearchTerm = ref('');
+    //
+    // const availableProjects = computed(function () {
+    //   if (activeSearchTerm.value) {
+    //     return props.user.projects.filter((prj) =>
+    //       prj.title.includes(activeSearchTerm.value)
+    //     );
+    //   }
+    //   return props.user.projects;
+    // });
 
     const hasProjects = computed(function () {
-      return props.user.projects && availableProjects.value.length > 0;
+      return props.user.projects && availableItems.value.length > 0;
     });
 
-    watch(enteredSearchTerm, function (newValue) {
-      setTimeout(() => {
-        if (newValue === enteredSearchTerm.value) {
-          activeSearchTerm.value = newValue;
-        }
-      }, 300);
-    });
+    // watch(enteredSearchTerm, function (newValue) {
+    //   setTimeout(() => {
+    //     if (newValue === enteredSearchTerm.value) {
+    //       activeSearchTerm.value = newValue;
+    //     }
+    //   }, 300);
+    // });
 
     // const propsWithRefs = toRefs(props);
     // const user = propsWithRefs.user;
-
-    const { user } = toRefs(props);
 
     watch(user, function () {
       enteredSearchTerm.value = '';
     });
 
-    function updateSearch(val) {
-      enteredSearchTerm.value = val;
-    }
+    // function updateSearch(val) {
+    //   enteredSearchTerm.value = val;
+    // }
 
     return {
       enteredSearchTerm,
-      availableProjects,
+      availableProjects: availableItems,
       hasProjects,
       updateSearch,
     };
