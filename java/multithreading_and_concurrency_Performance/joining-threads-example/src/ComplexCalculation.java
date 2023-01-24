9507fe470cc6d94f572686841a289cd2828dcdd9
loky13 <loky13@daum.net>
2023-01-24 23:54:03 +0900

    thread.join example
    https://www.udemy.com/course/java-multithreading-concurrency-performance-optimization/learn/lecture/16110999#overview

---
+++ java/multithreading_and_concurrency_Performance/joining-threads-example/src/ComplexCalculation.java
@@ -0,0 +1,50 @@
+import java.math.BigInteger;
+
+public class ComplexCalculation {
+    public BigInteger calculateResult(BigInteger base1, BigInteger power1, BigInteger base2, BigInteger power2) {
+        BigInteger result;
+        /*
+            Calculate result = ( base1 ^ power1 ) + (base2 ^ power2).
+            Where each calculation in (..) is calculated on a different thread
+        */
+
+        PowerCalculatingThread thread1 = new PowerCalculatingThread(base1, power1);
+        PowerCalculatingThread thread2 = new PowerCalculatingThread(base2, power2);
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        result = thread1.getResult().add(thread2.getResult());
+
+
+        return result;
+    }
+
+    private static class PowerCalculatingThread extends Thread {
+        private BigInteger result = BigInteger.ONE;
+        private BigInteger base;
+        private BigInteger power;
+
+        public PowerCalculatingThread(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+           /*
+           Implement the calculation of result = base ^ power
+           */
+            for( BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0;
+            i = i.add(BigInteger.ONE)) {
+                result = result.multiply(base);
+            }
+        }
+
+        public BigInteger getResult() { return result; }
+    }
+}
