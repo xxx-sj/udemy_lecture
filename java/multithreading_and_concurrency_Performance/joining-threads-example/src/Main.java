01b89d44c17992b9e7e886f2de3db15492c42362
loky13 <loky13@daum.net>
2023-01-24 23:46:39 +0900

    thread.join
    https://github.com/xxx-sj/Today_I_Learned/blob/master/java/multi_thread/joining_threads.md

--- java/multithreading_and_concurrency_Performance/joining-threads-example/src/Main.java
+++ java/multithreading_and_concurrency_Performance/joining-threads-example/src/Main.java
@@ -37,6 +37,7 @@ public class Main {
 
         List<FactorialThread> threads = new ArrayList<>();
 
+
         for (long inputNumber : inputNumbers) {
             threads.add(new FactorialThread(inputNumber));
         }
