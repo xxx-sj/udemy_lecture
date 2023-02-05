6c1facd60a3fbd32cb94a425ec71ddc0a3d092d8
loky13 <loky13@daum.net>
2023-02-06 00:28:56 +0900

    https://github.com/xxx-sj/Today_I_Learned/blob/master/java/multi_thread/Concurrency_challengse_and_solutions/atomic_operations_volatile_and_metircs.md

--- java/multithreading_and_concurrency_Performance/application-metrics-example/src/Main.java
+++ java/multithreading_and_concurrency_Performance/application-metrics-example/src/Main.java
@@ -95,6 +95,7 @@ public class Main {
         private long count = 0;
         private volatile double average = 0.0;
 
+        //capture method
         public synchronized void addSample(long sample) {
             double currentSum = average * count;
             count++;
@@ -104,5 +105,45 @@ public class Main {
         public double getAverage() {
             return average;
         }
+    }
+}
+
+
+
+public class MinMaxMetrics {
+
+    private volatile long minValue;
+    private volatile long maxValue;
+
+    /**
+     * Initializes all member variables
+     */
+    public MinMaxMetrics() {
+        this.maxValue = Long.MIN_VALUE;
+        this.minValue = Long.MAX_VALUE;
+    }
+
+    /**
+     * Adds a new sample to our metrics.
+     */
+    public void addSample(long newSample) {
+        synchronized (this) {
+            this.minValue = Math.min(newSample, this.minValue);
+            this.maxValue = Math.max(newSample, this.maxValue);
+        }
+    }
+
+    /**
+     * Returns the smallest sample we've seen so far.
+     */
+    public long getMin() {
+        return this.minValue;
+    }
+
+    /**
+     * Returns the biggest sample we've seen so far.
+     */
+    public long getMax() {
+        return this.maxValue;
     }
 }
