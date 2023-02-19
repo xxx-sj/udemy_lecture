3f32116249f2e57d5da950db044f8183035152a6
loky13 <loky13@daum.net>
2023-02-19 23:17:06 +0900

    code
    https://github.com/xxx-sj/Today_I_Learned/blob/master/java/multi_thread/Reentrant_lock/Reentrant_Read_write_lock_and_database_implementation.md

--- java/multithreading_and_concurrency_Performance/read-write-lock-example/src/Main.java
+++ java/multithreading_and_concurrency_Performance/read-write-lock-example/src/Main.java
@@ -37,11 +37,13 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         InventoryDatabase inventoryDatabase = new InventoryDatabase();
 
+        //10만번 반복하면서 item setting
         Random random = new Random();
         for (int i = 0; i < 100000; i++) {
             inventoryDatabase.addItem(random.nextInt(HIGHEST_PRICE));
         }
 
+        //writer thread 추가와 삭제를 진행한다. update item
         Thread writer = new Thread(() -> {
             while (true) {
                 inventoryDatabase.addItem(random.nextInt(HIGHEST_PRICE));
@@ -56,17 +58,21 @@ public class Main {
         writer.setDaemon(true);
         writer.start();
 
+        //reader thread 수
         int numberOfReaderThreads = 7;
         List<Thread> readers = new ArrayList<>();
 
+        //for문 돌면서 thread생성해서 읽게 시키기.
         for (int readerIndex = 0; readerIndex < numberOfReaderThreads; readerIndex++) {
             Thread reader = new Thread(() -> {
                 for (int i = 0; i < 100000; i++) {
                     int upperBoundPrice = random.nextInt(HIGHEST_PRICE);
                     int lowerBoundPrice = upperBoundPrice > 0 ? random.nextInt(upperBoundPrice) : 0;
+                    //range 갯수 세기
                     inventoryDatabase.getNumberOfItemsInPriceRange(lowerBoundPrice, upperBoundPrice);
                 }
             });
+            //빠르게 READ 하기 위해서 sleep 하지 않는다.
 
             reader.setDaemon(true);
             readers.add(reader);
@@ -77,6 +83,8 @@ public class Main {
             reader.start();
         }
 
+        //reader의 순서를 지키기위해 join 사용
+        //10만번씩 읽게 하기 위해서
         for (Thread reader : readers) {
             reader.join();
         }
@@ -86,28 +94,35 @@ public class Main {
         System.out.println(String.format("Reading took %d ms", endReadingTime - startReadingTime));
     }
 
+    //database class
     public static class InventoryDatabase {
+        //가격에 대한 item 수
         private TreeMap<Integer, Integer> priceToCountMap = new TreeMap<>();
         private ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
         private Lock readLock = reentrantReadWriteLock.readLock();
         private Lock writeLock = reentrantReadWriteLock.writeLock();
         private Lock lock = new ReentrantLock();
 
+        //read만 하는 method
         public int getNumberOfItemsInPriceRange(int lowerBound, int upperBound) {
             //lock.lock();
             readLock.lock();
             try {
+                //크거나 같은 트리에 존재하는 최저 가격 찾기
                 Integer fromKey = priceToCountMap.ceilingKey(lowerBound);
 
+                //높은 가격 찾기. 상한 가격보다 작거나 같음
                 Integer toKey = priceToCountMap.floorKey(upperBound);
 
                 if (fromKey == null || toKey == null) {
                     return 0;
                 }
 
+                //tree 의 스냅샷을 찍는다.
                 NavigableMap<Integer, Integer> rangeOfPrices = priceToCountMap.subMap(fromKey, true, toKey, true);
 
                 int sum = 0;
+                //item 수 더하기
                 for (int numberOfItemsForPrice : rangeOfPrices.values()) {
                     sum += numberOfItemsForPrice;
                 }
@@ -125,8 +140,10 @@ public class Main {
             try {
                 Integer numberOfItemsForPrice = priceToCountMap.get(price);
                 if (numberOfItemsForPrice == null) {
+                    //맵에 값이 없으면 새롭게 추가
                     priceToCountMap.put(price, 1);
                 } else {
+                    //값에 추가
                     priceToCountMap.put(price, numberOfItemsForPrice + 1);
                 }
 
@@ -136,14 +153,17 @@ public class Main {
             }
         }
 
+        //주어진 값에 더하기
         public void removeItem(int price) {
             //lock.lock();
             writeLock.lock();
             try {
                 Integer numberOfItemsForPrice = priceToCountMap.get(price);
                 if (numberOfItemsForPrice == null || numberOfItemsForPrice == 1) {
+                    //없거나 1개이면 삭제
                     priceToCountMap.remove(price);
                 } else {
+                    //1개 줄이기.
                     priceToCountMap.put(price, numberOfItemsForPrice - 1);
                 }
             } finally {
