58e7e1f76cca0728def45f978b396876289ab1ea
loky13 <loky13@daum.net>
2023-03-01 11:55:10 +0900

    modify
    https://github.com/xxx-sj/Today_I_Learned/blob/master/java/multi_thread/Lock-Free_%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98_%EB%8D%B0%EC%9D%B4%ED%84%B0_%EA%B5%AC%EC%A1%B0%EB%B0%8F_%EA%B8%B0%EC%88%A0/%EC%9B%90%EC%9E%90%EC%A0%81_%EB%A0%88%ED%8D%BC%EB%9F%B0%EC%8A%A4_compareAndSet_Lock-free_%EA%B3%A0%EC%84%B1%EB%8A%A5_%EB%8D%B0%EC%9D%B4%ED%84%B0%EA%B5%AC%EC%A1%B0.md

--- java/multithreading_and_concurrency_Performance/atomic-reference-example/src/Main.java
+++ java/multithreading_and_concurrency_Performance/atomic-reference-example/src/Main.java
@@ -35,8 +35,8 @@ import java.util.concurrent.locks.LockSupport;
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        //StandardStack<Integer> stack = new StandardStack<>();
-        LockFreeStack<Integer> stack = new LockFreeStack<>();
+        //StandardStack<Integer> stack = new StandardStack<>(); //1억5천
+        LockFreeStack<Integer> stack = new LockFreeStack<>();// 5억6천  3배 차이.
         Random random = new Random();
 
         for (int i = 0; i < 100000; i++) {
@@ -74,44 +74,83 @@ public class Main {
             thread.start();
         }
 
+        //10초 동안 push and pop 하고
         Thread.sleep(10000);
 
+        //얼마나 많은 수행이 있었는지 counter를 print 한다.
         System.out.println(String.format("%,d operations were performed in 10 seconds ", stack.getCounter()));
     }
 
+    /**
+     * push, pop: 헤드의 현재값을 읽은 다음 해당 값을 기반으로 헤등를 대체할
+     * 새 후보자 값을 계산하는데 그러면서 방금 읽은 값이 변하지 않았기를 기원하는 것.
+     * 여기서 compareAndSet 메서드를 써서 예상값이 여전히 유요한지 확인하고
+     * 헤드의 값을 그 새 후보자로 설정하는 것이다.
+     * 만약 변하였다면, false를 반환하고 성공할때 까지 반복하는 것이다. [while]
+     *
+     */
     public static class LockFreeStack<T> {
+        //atomicReference
         private AtomicReference<StackNode<T>> head = new AtomicReference<>();
         private AtomicInteger counter = new AtomicInteger(0);
 
         public void push(T value) {
             StackNode<T> newHeadNode = new StackNode<>(value);
 
+            //반복적으로 생성하는 연산
+            /**
+             * loop가 필요한 이유는 많은 스레드가 동시에 스택으로 항목을
+             * 푸시하거나 또는 스택에서 팝하려고 할 거라서 성공하려면 몇 번 정도
+             * 시도를 해야하기 때문이다.
+             */
             while (true) {
+                //원자적 레퍼런스인 헤드가 그 시점에 가리키고 있는 currentHeadNode
+                //stack에 newHead 넣기전.
                 StackNode<T> currentHeadNode = head.get();
+                //새로운 newHead의 next에 현재 head값을 next에 저장.
                 newHeadNode.next = currentHeadNode;
+                //원자적 레퍼런스인 헤드는 현재 currentHeadNode를 가리키고 있음
                 if (head.compareAndSet(currentHeadNode, newHeadNode)) {
+                    //condition이 true라면 head를 newHead 값으로 변경하고 while을 빠져나간다.
+                    //true라는 것은 어떠한 thread에 의해서 값이 변경되지 않았다는 것이다.
                     break;
                 } else {
+                    //false를 반환하면 헤드를 읽는 연산[heae]과 newHeadNode로 헤드를
+                    //업데이트 하는 연산[head.compareAndSet(currentHeadNode, newHeadNode)]
+                    // 사이에 헤드가 변경되었다는 뜻으로 그 모든 연산을 다시 수행해야 하는데
+                    // 다시 시도하기 전에 1ns 기다리고 다시 진행한다.
                     LockSupport.parkNanos(1);
                 }
             }
+            //원자적integer로 선언하여 counter를 증가시키는 구문을 원자적연산으로 변경
             counter.incrementAndGet();
         }
 
         public T pop() {
+            //pop하려는 현재 노드를 get으로 가져온다.
             StackNode<T> currentHeadNode = head.get();
+            //pop되고 나서의 새로운 head 노드를 나타낼 값.
             StackNode<T> newHeadNode;
 
+            //while loop는 현재 헤드가 null이 아닐때만 돌아간다.
             while (currentHeadNode != null) {
+                //현재 노드의 다음값을 새로운 헤드값으로 할당하고
                 newHeadNode = currentHeadNode.next;
+                //헤드의 값이 get()으로 얻은 현재 헤드 노드 값과 일치한다면
                 if (head.compareAndSet(currentHeadNode, newHeadNode)) {
+                    //newHeadNode로 head의 값을 변경하고 break; 한다.
                     break;
                 } else {
+                    //false라면 head의 값과 currentHeadNode snapshot의 값이
+                    //일치 하지 않다는 것으로 다른 스레드에 의해 값이 변경되었다는 뜻으로
+                    //1ns 기다린후 head에서 get을 통해 현재 헤드노드의 snapshot 값을 변경한다.
                     LockSupport.parkNanos(1);
                     currentHeadNode = head.get();
                 }
             }
+            //counter의 원자적 연산
             counter.incrementAndGet();
+            //pop의 return으로 현재 노드가 null이 아니라면 value값을 null이라면 null 반환.
             return currentHeadNode != null ? currentHeadNode.value : null;
         }
 
@@ -120,25 +159,38 @@ public class Main {
         }
     }
 
+    //동기화를 쓰지 않으면 여러 경쟁 상태가 가능해진다.
     public static class StandardStack<T> {
         private StackNode<T> head;
         private int counter = 0;
 
         public synchronized void push(T value) {
+            //새로운 head를 생성 가장 위에 쌓일 값.
             StackNode<T> newHead = new StackNode<>(value);
+            //=================race condition======================
+            //newhead의 다음 값으로 이전 헤드값 저장
             newHead.next = head;
+            //헤드에 newHead [새로운 헤드] 값 저장
             head = newHead;
+            //=======================================
+            //counter 증가
             counter++;
         }
 
         public synchronized T pop() {
+            //head가 null 이라면.
             if (head == null) {
+                //counter만 증가 시키고 null 반환.
                 counter++;
                 return null;
             }
-
+            //===============race condition========================
+            //null이 아니라면 head의 값을 스냅샷 찍어서
             T value = head.value;
+            //헤드의 다음값을 헤드로 변경 [한칸을 내린다.]
             head = head.next;
+            //카운터 증가시키고 value 반환.
+            //=======================================
             counter++;
             return value;
         }
@@ -148,13 +200,14 @@ public class Main {
         }
     }
 
+    //연결 리스트 value node에 해당하는 값
     private static class StackNode<T> {
         public T value;
         public StackNode<T> next;
 
         public StackNode(T value) {
             this.value = value;
-            this.next = next;
+            this.next = next; //현재 노드의 다음 값. stack의 node 아래 값.
         }
     }
 }
